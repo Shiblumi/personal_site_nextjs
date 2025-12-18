4c9f01533a4f4ad1577cfbe00abf49d0655a1a46
Shiblumi <70780517+Shiblumi@users.noreply.github.com>
2025-12-18 11:43:40 -0800

    Add clickable timeline dot navigation.

--- src/components/UI/ScrollingTimeline/ScrollingTimeline.js
+++ src/components/UI/ScrollingTimeline/ScrollingTimeline.js
@@ -3,27 +3,25 @@ import { useNavbarContext } from '@/components/UI/ActiveSectionContext';
 import { memo } from 'react';
 import styles from './ScrollingTimeline.module.css';
 
-const TimelineDot = memo(({ isActive }) => {
+const TimelineDot = memo(({ isActive, href }) => {
 	return (
-		<motion.div
+		<motion.a
 			className={styles['timeline-dot']}
 			initial={{ scale: 0.8, opacity: 0.4 }}
 			animate={{
 				scale: isActive ? 1.2 : 0.8,
 				opacity: isActive ? 1 : 0.4,
-				boxShadow: isActive 
+				boxShadow: isActive
 					? [
-						'0 0 10px rgba(var(--logo-rgb), 0.6)',
-						'0 0 20px rgba(var(--logo-rgb), 0.4)', 
-						'0 0 30px rgba(var(--logo-rgb), 0.2)'
-					].join(', ')
+							'0 0 10px rgba(var(--logo-rgb), 0.6)',
+							'0 0 20px rgba(var(--logo-rgb), 0.4)',
+							'0 0 30px rgba(var(--logo-rgb), 0.2)',
+					  ].join(', ')
 					: 'none',
-				borderColor: isActive 
-					? 'rgb(var(--logo-rgb))' 
+				borderColor: isActive
+					? 'rgb(var(--logo-rgb))'
 					: 'rgba(var(--logo-rgb), 0.6)',
-				backgroundColor: isActive 
-					? 'rgb(var(--logo-rgb))' 
-					: 'transparent',
+				backgroundColor: isActive ? 'rgb(var(--logo-rgb))' : 'transparent',
 			}}
 			transition={{
 				duration: 0.5,
@@ -39,21 +37,29 @@ export default function ScrollingTimeline() {
 	const { activeSection } = useNavbarContext();
 
 	const sections = [
-		{ id: 1, name: 'home' },
-		{ id: 2, name: 'exp' },
-		{ id: 3, name: 'skills' },
-		{ id: 4, name: 'projects' },
-		{ id: 5, name: 'contact' },
+		{ id: 1, href: '#home', title: 'Home' },
+		{ id: 2, href: '#exp', title: 'Exp' },
+		{ id: 3, href: '#skills', title: 'Skills' },
+		{ id: 4, href: '#projects', title: 'Projects' },
+		{ id: 5, href: '#contact', title: 'Contact' },
 	];
 
 	return (
 		<div className={`${styles['section-timeline-wrapper']}`}>
 			{sections.map((section) => (
-				<TimelineDot 
-					key={section.id}
-					section={section}
-					isActive={activeSection === section.id}
-				/>
+				<a
+					className={`${styles['timeline-dot-wrapper']}`}
+					key={`Dot-Wrapper-${section.id}`}
+					href={section.href}
+					title={section.title}
+				>
+					<TimelineDot
+						key={section.id}
+						href={section.href}
+						section={section}
+						isActive={activeSection === section.id}
+					/>
+				</a>
 			))}
 		</div>
 	);
